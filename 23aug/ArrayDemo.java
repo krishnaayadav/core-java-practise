93fed04f644e4f875db9e5148adf746b571678a6
krishna yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-08-24 11:14:44 +0530

    Add files via upload

---
+++ 23aug/ArrayDemo.java
@@ -0,0 +1,19 @@
+class ArrayDemo
+{
+  static void printArray()
+  {
+	  int []MyArray= new int[3];
+	  MyArray[0]=45;
+	  MyArray[1]=78;
+	  
+	   System.out.println("first element:" +MyArray[0]);
+	   System.out.println("second element:" +MyArray[1]);
+  }
+
+  
+        public static void main(String args[])
+	{
+         printArray();
+	}
+}
+
