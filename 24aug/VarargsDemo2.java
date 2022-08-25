e50c1357af50122de5629ba7032ab68ac60fcf63
Krishna Yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-08-25 10:55:05 +0530

    Add files via upload

---
+++ 24aug/VarargsDemo2.java
@@ -0,0 +1,21 @@
+/*
+Program: VarargsDemo2
+@author: krishna yadav
+*/
+
+class VarargsDemo2
+{
+	
+  public static void main(String ...args)
+  {
+   
+    for(String i:args)
+    {
+		System.out.println(i);
+	}		
+
+    
+	
+  }
+
+}
