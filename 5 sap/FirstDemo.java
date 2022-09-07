accdb7d11e0adee7a3b82d7afaa8a970e63cc261
Krishna Yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-09-07 16:46:32 +0530

    Add files via upload

---
+++ 5 sap/FirstDemo.java
@@ -0,0 +1,7 @@
+class FirstDemo // calling class name 
+{ 
+public static void main(String [] args)    // calling main method 
+ {
+    System.out.println("hello world");    //printing a new line
+	}       // end of main class
+  }        // end of class firstdemo 
