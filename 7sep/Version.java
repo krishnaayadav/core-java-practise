c430ca29cefa9e6f9e774fc5934aab197bf67909
Krishna Yadav <111423042+krishnaayadav@users.noreply.github.com>
2022-09-09 11:04:58 +0530

    Add files via upload

---
+++ 7sep/Version.java
@@ -0,0 +1,15 @@
+ /* 
+@PROGRAM: Written a program to print the version of java .
+@author: krishna yadav
+@date:  7 sep 2022
+*/
+ 
+
+
+class Version // calling class name 
+{ 
+public static void main(String [] args)    // calling main method 
+ {
+   System.out.println("java version is :"+ System.getProperty("java.version")); //printing version of java using keyword
+  }
+ }
